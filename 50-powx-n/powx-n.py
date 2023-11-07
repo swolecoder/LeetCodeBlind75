3f1ae81506f0579cb382422dab4bf0ed27194f02
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2023-11-06 20:57:34 -0800

    Time: 36 ms (71.79%) | Memory: 16.3 MB (12.22%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,29 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+
+        # 2 ^ 10 = 2 ^ 5 * 2^ 5
+
+        # 2 ^ 5 = 2 * 2 ^ 4
+
+        # 2 ^ 4 = 2 ^ 2 * 2 ^ 2
+
+        # 2 ^ 2 = 2 ^ 1 + 2 ^ 1
+
+        # 2 ^ 1 = 2 ^ 0 + 2 ^ 0 = 1
+
+
+        def helper(x, n):
+
+            if n <= 0:
+                return 1
+            
+            if n % 2 == 0:
+                temp = helper(x, n//2)
+                return temp * temp
+            else: 
+                return x * helper(x, n-1)
+        
+        return 1 /helper(x,abs(n)) if n < 0 else helper(x,abs(n))
+        
+
+        
