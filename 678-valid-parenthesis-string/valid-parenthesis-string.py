506340547838e5a6998193abb72ca888af292c96
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2023-12-23 08:39:53 -0800

    Time: 38 ms (58.48%) | Memory: 17.4 MB (6.49%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,42 +1,61 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
 
-        open = []
-        star = []
-
+        leftMax = 0
+        leftMin = 0
 
         for i in range(len(s)):
-
             if s[i] == "(":
-                open.append(i)
+                leftMax +=1
+                leftMin +=1
             elif s[i] == ")":
-                if open:
-                    open.pop()
-                else:
-
-                    if not open and not star:
-                        return False
-                    elif star:
-                        star.pop()
-
-
+                leftMax -=1
+                leftMin = max( leftMin -1 , 0)
             else:
-                star.append(i)
-        
-        # balnce the open ones
-
-        while open:
-            if not star:
-                return False
+                leftMax +=1
+                leftMin = max( leftMin -1 , 0)
             
-            o1 = open.pop()
-            s1= star.pop()
-
-            if o1 > s1:
+            if leftMax < 0:
                 return False
+        print(leftMax, leftMin)
+        return not leftMax or not leftMin
+
+        # open = []
+        # star = []
+
+
+        # for i in range(len(s)):
+
+        #     if s[i] == "(":
+        #         open.append(i)
+        #     elif s[i] == ")":
+        #         if open:
+        #             open.pop()
+        #         else:
+
+        #             if not open and not star:
+        #                 return False
+        #             elif star:
+        #                 star.pop()
+
+
+        #     else:
+        #         star.append(i)
         
-        print(open, star)
-        return True
+        # # balnce the open ones
+
+        # while open:
+        #     if not star:
+        #         return False
+            
+        #     o1 = open.pop()
+        #     s1= star.pop()
+
+        #     if o1 > s1:
+        #         return False
+        
+        # print(open, star)
+        # return True
 
 
         
