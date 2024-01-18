ff37998fbabdf2d909463d556ed61722bdbe98bb
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-01-17 18:08:28 -0800

    Time: 465 ms (47.50%) | Memory: 24.7 MB (15.89%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,24 +1,28 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-
         seen = set()
+        R = len(grid)
+        C = len(grid[0])
+
 
         def dfs(r,c):
-            if r >= len(grid) or c >= len(grid[0]) or r < 0 or c < 0 or grid[r][c] == 0:
+            if r < 0 or c < 0 or r >= R or c >= C or grid[r][c] == 0:
                 return 1
-
-            if (r,c) in seen:
-                return 0
-
+            
+            if (r,c) in seen :
+                return 0 
             seen.add((r,c))
 
-            perm = dfs(r+1,c) 
-            perm += dfs(r -1,c)
-            perm += dfs(r,c+1)
-            perm += dfs(r,c-1)
-            return perm
+            ans =  dfs(r -1,c)
+            ans += dfs(r+1,c)
+            ans += dfs(r, c-1)
+            ans += dfs(r,c+1)
+            return ans
+        
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(R):
+            for j in range(C):
                 if grid[i][j]:
-                    return dfs(i,j)      
+                    return dfs(i,j)
+        
+        
