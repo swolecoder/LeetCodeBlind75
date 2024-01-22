467203fc94dafc012bcd8dedcc3eeaa31dd86382
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-01-22 12:41:04 -0800

    Time: 118 ms (96.64%) | Memory: 18.2 MB (55.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l =0
-
-        for r in range(len(nums)):
-            if nums[r]:
-                nums[l], nums[r] = nums[r], nums[l]
-                l +=1
-
+        r = 0
+        for i in range(len(nums)):
+            if nums[i]:
+                nums[r], nums[i] = nums[i], nums[r]
+                r +=1
         
         
