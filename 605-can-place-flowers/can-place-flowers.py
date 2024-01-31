e1420bd61518c529c05d0152da78fc62949540d9
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-01-31 08:47:16 -0800

    Time: 140 ms (50.29%) | Memory: 17 MB (61.21%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -3,8 +3,10 @@ class Solution:
         f = [0] + flowerbed + [0]
 
         for i in range(1, len(f)-1):
-            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0 and n >0:
+            if not f[i-1] and not f[i] and not f[i+1] and n:
                 f[i] = 1
                 n -=1
-        
+                if n == 0:
+                    break
         return n == 0
+        
