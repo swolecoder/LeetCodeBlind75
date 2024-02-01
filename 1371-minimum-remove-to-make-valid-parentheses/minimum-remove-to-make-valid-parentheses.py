2a82aa90869409b6ffa1b598d91ae068c70b5aed
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-02-01 04:15:18 -0800

    Time: 74 ms (75.33%) | Memory: 18.2 MB (80.09%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,20 +1,36 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
-        listS = list(s)
+        # stack = []
+        # listS = list(s)
 
-        for i in range(len(listS)):
-            if listS[i] == "(":
-                stack.append(i)
-            elif listS[i] ==")":
-                if stack:
-                    stack.pop()
-                else:
-                    listS[i] = ""
+        # for i in range(len(listS)):
+        #     if listS[i] == "(":
+        #         stack.append(i)
+        #     elif listS[i] ==")":
+        #         if stack:
+        #             stack.pop()
+        #         else:
+        #             listS[i] = ""
 
-        for i in range(len(stack)):
-            listS[stack[i]] = ""
+        # for i in range(len(stack)):
+        #     listS[stack[i]] = ""
         
-        return ''.join(listS)
+        # return ''.join(listS)
+
+        count = 0
+        s = list(s)
+        for i in range(len(s)):
+            if s[i] == "(":
+                count +=1
+            elif s[i] == ")":
+                if count == 0:
+                    s[i] = ""
+                else:
+                    count -=1
+        for i in range(len(s)-1,-1,-1):
+            if s[i] == "(" and count > 0:
+                s[i] = ""
+                count -=1
+        return ''.join(s)
         
         
