c540a53a5dbdb642608ee2aa5d50077a1f888370
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-02-13 20:11:25 -0800

    Time: 812 ms (26.28%) | Memory: 27.3 MB (85.32%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minBuy = prices[0]
+
+        minTracker = prices[0]
+        
         ans = 0
 
         for i in range(len(prices)):
-            diff = prices[i] - minBuy 
-            if diff >0:
-                ans = max(ans, diff)
-            
-            minBuy = min(minBuy, prices[i])
+            diff = prices[i] - minTracker
+            ans = max(diff, ans)
+            minTracker = min(minTracker,prices[i])
         return ans
-        
