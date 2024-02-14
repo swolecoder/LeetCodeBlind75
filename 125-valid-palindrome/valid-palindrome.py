bc84a77f6be1cf2ddeebbc974b6876dededbaf7d
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-02-13 16:35:00 -0800

    Time: 43 ms (73.75%) | Memory: 17 MB (96.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
         i = 0
         j = len(s)-1
 
         while i < j:
-
-            while i < len(s) and i < j and not s[i].isalnum():
+            if not s[i].isalnum():
                 i +=1
-            while j > 0 and j > i  and not s[j].isalnum():
+                continue
+            elif not s[j].isalnum():
                 j -=1
-            
-            if s[i].lower() != s[j].lower():
-                return False
+                continue
+            else:
+                if s[i].lower() != s[j].lower():
+                    print("Ashish", i , j, s[i],s[j])
+                    return False
             
             i +=1
             j -=1
-        
-        return True
-        
+        return  True
+            
