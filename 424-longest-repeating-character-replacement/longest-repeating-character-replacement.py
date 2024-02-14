1576d98e9447df04092525379dd6311740556cc8
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-02-13 20:44:57 -0800

    Time: 95 ms (58.29%) | Memory: 16.6 MB (72.71%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,18 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         map = {}
-        l =0
+        l = 0
         res = 0
 
+        for i in range(len(s)):
+            map[s[i]] = map.get(s[i], 0) + 1
 
-        for r in range(len(s)):
-            map[s[r]] = map.get(s[r], 0) + 1
-
-            if (r - l +1) - max(map.values()) > k:
+            if (i - l +1) - max(map.values()) > k:
                 map[s[l]] = map[s[l]] -1
                 l +=1
             
-            res = max(res , r-l +1)
-        
-        return res
+            res = max(res, i-l +1)
+        return res 
         
