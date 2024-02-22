c2e52e40973c67a8f11f6a7ad4370e5491948f56
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-02-22 09:35:39 -0800

    Time: 100 ms (26.33%) | Memory: 22.3 MB (21.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,14 @@
-import collections
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashMap = collections.defaultdict(list)
+        map = defaultdict(list)
 
-        for i in range(len(strs)):
-             key = [0] * 26
+        for word in strs:
+            sort = [0] * 26
+            for j in range(len(word)):
+                 key  = ord(word[j]) - ord('a')
+                 sort[key] +=1
+            map[tuple(sort)].append(word)
+        return map.values()
 
-             for j in range(len(strs[i])):
-                 key[ord(strs[i][j]) - ord('a')] +=1
-             
-             hashMap[tuple(key)].append(strs[i])
         
-        print(hashMap)
-        return hashMap.values()
