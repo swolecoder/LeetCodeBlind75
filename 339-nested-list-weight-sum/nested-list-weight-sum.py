afb5171e42303a3166f35acd4cfa5fab9e3d5871
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-02-26 11:54:48 -0800

    Time: 42 ms (20.85%) | Memory: 16.7 MB (84.92%) - LeetSync

--- 339-nested-list-weight-sum/nested-list-weight-sum.py
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -40,20 +40,46 @@
 #        Return None if this NestedInteger holds a single integer
 #        :rtype List[NestedInteger]
 #        """
-
+from collections import deque
 class Solution:
     def __init__(self):
         self.ans = 0
+
     def depthSum(self, nestedList: List[NestedInteger]) -> int:
-        self.depth(nestedList)
-        return self.ans
+        # self.depth(nestedList)
+        # return self.ans
 
-    def depth(self, nums, level=1):
+        ans = 0
+        q = nestedList
+        level = 1
+        while q:
 
-        for n in nums:
-            if n.isInteger():
-                self.ans += n.getInteger() * level
-            else:
-                self.depth(n.getList(), level + 1)
+            nextItem = []
+            length = len(q)
+            for i in range(length):
+                node = q.pop(0)
+                if node.isInteger():
+                    ans += node.getInteger() * level 
+                else:
+                    nextItem.extend(node.getList())
+            
+            level +=1
+            q = nextItem
+        return ans
+
+
+
+
+
+
+
+
+    # def depth(self, nums, level=1):
+
+    #     for n in nums:
+    #         if n.isInteger():
+    #             self.ans += n.getInteger() * level
+    #         else:
+    #             self.depth(n.getList(), level + 1)
 
         
