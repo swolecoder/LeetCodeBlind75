ae5215b74d0fa5dacdf25e66c715b22a463cc949
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-03-01 08:46:40 -0800

    Time: 403 ms (11.19%) | Memory: 16.5 MB (88.09%) - LeetSync

---
+++ 465-optimal-account-balancing/optimal-account-balancing.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+class Solution:
+    def minTransfers(self, transactions: List[List[int]]) -> int:
+        map = defaultdict(int)
+
+        for frm, to, amount in transactions:
+            map[frm] -= amount
+            map[to] += amount
+        print(map)
+
+        debts = [amount for amount in map.values() if amount != 0]
+        print(debts)
+        
+
+        def dfs(data):
+            while data and data[0] == 0:
+                data.pop(0)
+            if not data:
+                return 0
+            if len(data) == 1 and data[0] ==0:
+                return 0
+            
+            count = float("inf")
+
+            for i in range(1,len(data)):
+                prev = data[0]
+
+                if prev * data[i] < 0:
+                    data[i] +=  data[0]
+                    best = dfs(data[1:])
+                    count = min(best, count)
+                    # restore
+                    data[i]-= data[0]
+            return 1 + count 
+        return dfs(debts)
+
+
+
+
+        
