bc763060a7486d4c100757bf5f482df625b69aef
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-03-05 10:11:37 -0800

    Time: 607 ms (22.43%) | Memory: 78.1 MB (37.69%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,32 +1,20 @@
-class Node:
-    def __init__(self, key=0, val=0):
+class DLL:
+    def __init__(self,key,val,left=None,prev=None):
         self.key = key
         self.val = val
-        self.next = None
-        self.Prev = None
+        self.left = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.c = capacity
-        self.cache = {}
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
-        self.head.next = self.tail
+        self.capacity = capacity
+        self.map = {}
+        self.head = DLL(-1,-1)
+        self.tail = DLL(-1,-1)
+        self.head.next =self.tail
         self.tail.prev = self.head
-    
-    # remove node from  list
-    def delete(self,node):
-        current = node
-        nextNode = node.next
-        prevNode = node.prev
-
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-
-
-    
-    #  add to right of list
+        
     def insert(self, node):
         current = node
         # prev element from tal 
@@ -36,38 +24,38 @@ class LRUCache:
         current.prev = prevElement 
         current.next = self.tail
         self.tail.prev = current
-        
-        
+
+    
+    def delete(self,node):
+        prev = node.prev
+        nextNode = node.next
+
+        prev.next = nextNode
+        nextNode.prev = prev
 
     def get(self, key: int) -> int:
-
-        if key in self.cache:
-            # remmove from list 
-            self.delete(self.cache[key])
-
-            # add to right most position
-            self.insert(self.cache[key])
-            # move this to right
-            return self.cache[key].val
+        if key in self.map:
+            # delete 
+            node = self.map[key]
+            self.delete(node)
+            self.insert(node)
+            return self.map[key].val
         return -1
+            
         
 
     def put(self, key: int, value: int) -> None:
 
-        if key in self.cache:
-            self.delete(self.cache[key])
-        # create a new Node with key and valuer
-        self.cache[key] = Node(key,value)
-        self.insert(self.cache[key])
+        if key in self.map:
+            self.delete(self.map[key])
+        
+        self.map[key] = DLL(key,value)
+        self.insert(self.map[key])
 
-        if len(self.cache) > self.c:
-            # delete or evict from left and del lru from cache, thats why we habe left pointer
+        if len(self.map) > self.capacity:
             lru = self.head.next
             self.delete(lru)
-            del self.cache[lru.key]
-
-
-
+            del self.map[lru.key]
         
 
 
