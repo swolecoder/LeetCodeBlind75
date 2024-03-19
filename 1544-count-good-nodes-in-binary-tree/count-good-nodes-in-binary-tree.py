773a9b3ccd54672fdaecdb443c4f07098c8a04c6
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-03-19 10:46:23 -0700

    Time: 151 ms (16.02%) | Memory: 31 MB (73.37%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -7,21 +7,25 @@
 from collections import deque
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        
-        q = deque([(root, float("-inf"))])
 
         ans = 0
-        while q:
-            node, prevMax = q.popleft()
+        q = deque([(root, root.val)])
 
-            if node.val >= prevMax:
+        while q:
+
+            node,maxVal = q.popleft()
+
+            if node.val >= maxVal:
                 ans +=1
             
             if node.left:
-                q.append((node.left, max(node.val, prevMax)))
+                q.append((node.left, max(node.val,maxVal)))
             if node.right:
-                q.append((node.right, max(node.val, prevMax)))
+                q.append((node.right, max(node.val,maxVal)))
+
+
+
+
         
-        return ans 
+        return ans
+        
