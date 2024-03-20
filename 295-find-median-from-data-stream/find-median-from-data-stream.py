d7d848690dea929e7754ecbd7628d40e7000ea68
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-03-20 10:43:27 -0700

    Time: 358 ms (82.52%) | Memory: 38.5 MB (44.08%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,30 +1,31 @@
-from heapq import heappush , heappop, heapify
+from heapq import heappush, heappop,heapify
 class MedianFinder:
 
     def __init__(self):
         self.max = []
         self.min = []
-
         
 
     def addNum(self, num: int) -> None:
-        #bit eempty
-        if not self.max or num <= -1* self.max[0]:
-            heappush(self.max,-1* num)
+        if not self.max or num <= -1 * self.max[0]:
+            heappush(self.max, -num)
         else:
             heappush(self.min, num)
         
         if len(self.max) - len(self.min) > 1:
-            popData = heappop(self.max)
-            heappush(self.min,-1 * popData)
+            popdata = heappop(self.max)
+            heappush(self.min, -1 * popdata)
         elif len(self.min) - len(self.max) > 1:
-            popData = heappop(self.min)
-            heappush(self.max,-1 * popData)
+            popdata = heappop(self.min)
+            heappush(self.max, -1 * popdata)
+
+
+        
 
     def findMedian(self) -> float:
         if len(self.max) > len(self.min):
             return -self.max[0]
-        elif len(self.max) < len(self.min):
+        elif len(self.min) > len(self.max):
             return self.min[0]
         else:
             return (-self.max[0] + self.min[0]) / 2.0
