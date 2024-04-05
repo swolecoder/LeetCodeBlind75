075f319038a11852041d643a49d49643e05750dd
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-05 11:14:20 -0700

    Time: 59 ms (66.09%) | Memory: 17.9 MB (9.51%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,12 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
+        hashMap = defaultdict(int)
 
         for i in range(len(nums)):
-            y = target -nums[i] 
-
-            if y in map:
-                return [map[y], i]
-            map[nums[i]] = i
-        return [-1,-1]
+            y = target  - nums[i]
+            if y in hashMap:
+                return [hashMap[y], i]
+            hashMap[nums[i]] = i
         
+        return [-1,-1]
