61f77ddb0cc80976b8217089ce5ae0e68066dd7e
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-06 12:07:13 -0700

    Time: 38 ms (58.97%) | Memory: 16.8 MB (8.22%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,40 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()
+
+        if not s:
+            return 0
+
+        negative = False
+        i = 0
+        if s and s[i] == "+" or s[i] == "-":
+            if s[i] == "-":
+                negative = True
+            i +=1
+        
+     
+        
+        parse = 0
+        while i < len(s):
+            if s[i].isdigit():
+                parse = parse * 10 + int(s[i])
+                i +=1
+            else:
+                break
+        
+        print(parse)
+        if negative:
+            parse = -1 * parse
+
+        if parse > pow(2, 31) -1:
+            return pow(2, 31) -1
+        if parse < -1*pow(2, 31):
+            return -1*pow(2, 31)
+        return  parse
+
+
+            
+
+
+
+        
