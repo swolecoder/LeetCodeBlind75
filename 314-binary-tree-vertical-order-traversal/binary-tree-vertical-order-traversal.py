de836c7b234e61ba19c7c4a206e078e3957ff23e
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-07 14:40:14 -0700

    Time: 36 ms (69.29%) | Memory: 16.5 MB (92.92%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -4,22 +4,49 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import defaultdict, deque
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
         map = defaultdict(list)
-
+        minValue = float("inf")
+        maxValue = float("-inf")
         if not root:
             return []
 
-        q = [(root, 0)]
 
-        while q:
-            node, level = q.pop(0)
-            map[level].append(node.val)
-            if node.left:
-                q.append((node.left, level -1))
-            if node.right:
-                q.append((node.right, level+1))
-        return [ map[data] for data in sorted(map.keys())]
+        def bfs(node, level):
+            q = deque([(node, level)])
+            nonlocal minValue 
+            nonlocal maxValue 
+
+            # if not node:
+            #     return
+            
+            # minValue = min(minValue, level)
+            # maxValue = max(maxValue, level)
+            # map[level].append(node.val)
+            # dfs(node.left, level-1)
+            # dfs(node.right, level +1)
+
+            while q:
+                n, l = q.popleft()
+                minValue = min(minValue, l)
+                maxValue = max(maxValue, l)
+                map[l].append(n.val)
+
+                if n.left:
+                    q.append((n.left, l -1))
+                if n.right:
+                    q.append((n.right, l +1))
+
+
         
+        bfs(root, 0)
+        ans = []
+
+        for i in range(minValue, maxValue+1):
+            if i in map:
+                ans.append(map[i])
+        return ans
+
