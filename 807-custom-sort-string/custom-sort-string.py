994090c675449ef59b1006d470473334e559b392
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-09 16:43:49 -0700

    Time: 37 ms (47.42%) | Memory: 16.5 MB (49.65%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,22 +1,23 @@
-import collections
+from collections import defaultdict
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        hashMap = collections.defaultdict(int)
+        map = defaultdict(int)
+
         for ch in s:
-            hashMap[ch] +=1
+            map[ch] +=1
+        print(map)
+
+
+        res = []
+
+        for c in order:
+            if c in map:
+                res.extend( c * map[c])
+                del map[c]
+        print(res)
+
+        for k, v in map.items():
+            res.extend((k* v))
         
-        print(hashMap)
-        ans = []
-
-        for ch in order:
-            if ch in hashMap:
-                ans.extend((ch * hashMap[ch]))
-                del hashMap[ch]
-        print(ans)
-
-        for k, v in hashMap.items():
-            ans.extend((k*v))
-        
-        return ''.join(ans)
-
+        return ''.join(res)
         
