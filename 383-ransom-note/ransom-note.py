fd5708aea3711b395ea9c3523f167846a5367f49
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-15 09:51:59 -0700

    Time: 52 ms (71.88%) | Memory: 16.8 MB (45.19%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashMap= Counter(magazine)
+
+        a: 2
+        b :1
+
+        for ch in ransomNote:
+            if ch not in hashMap:
+                return False
+            else:
+                hashMap[ch] -=1
+                if hashMap[ch] == 0:
+                    del hashMap[ch]
+        return True
+
+
+        
