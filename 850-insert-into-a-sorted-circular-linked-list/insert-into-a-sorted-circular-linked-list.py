6b75737ef506cde688e5dc95c2b69b2476792b16
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-15 09:30:22 -0700

    Time: 37 ms (67.50%) | Memory: 17.2 MB (88.37%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.py
@@ -8,36 +8,48 @@ class Node:
 
 class Solution:
     def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
-        #no head
+
         if not head:
             node = Node(insertVal)
             node.next = node
             return node
         
-       
         curr = head
 
         while curr.next != head:
 
-            if curr.val <= insertVal and curr.next.val >= insertVal:
+            if curr.val <= insertVal and insertVal <= curr.next.val:
                 node = Node(insertVal)
-                node.next = curr.next
+                temp = curr.next
                 curr.next = node
+                node.next = temp
+                
                 return head
+            
             elif curr.val > curr.next.val:
-
-                if insertVal > curr.val or insertVal < curr.next.val:
+                if insertVal < curr.next.val or insertVal > curr.val:
                     node = Node(insertVal)
-                    node.next = curr.next
+                    temp = curr.next
                     curr.next = node
+                    node.next = temp
                     return head
-            curr = curr and curr.next 
+            
+            curr = curr and curr.next
         
         node = Node(insertVal,curr.next)
         curr.next = node
+        # node.next = curr
         return head
 
 
 
 
-        
+
+
+
+
+
+
+
+
+
