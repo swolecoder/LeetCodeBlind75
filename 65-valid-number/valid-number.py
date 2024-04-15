8d79d46165dc75f11398dbc99ea10ff78d07d68f
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-15 08:37:48 -0700

    Time: 35 ms (75.45%) | Memory: 16.5 MB (93.70%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -1,52 +1,33 @@
 class Solution:
-    def validate(self, s):
-        if not s:
-            return False
-        digit_seen = False
-        sign_seen = False
-
-        i = 0 
-        if s[i] in ["+","-"]:
-            i +=1
-        
-        while i < len(s):
-            if s[i] in ["+", "-"]:
-                return False
-            else:
-                if not s[i].isdigit():
-                    return False
-                digit_seen= True
-            i +=1
-        return digit_seen
     def isNumber(self, s: str) -> bool:
-        # ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"]
+        #["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"]
 
-        decimal_seen = False
-        digit_seen = False
-        sign_seen = False
-        i = 0
-        if s[i] in ["+","-"]:
-            i +=1
-            sign_seen = True
+        digit,decimal, e, symbol = False, False, False, False
+
+        for ch in s:
+            if ch in "0123456789":
+                digit = True
         
-        while i < len(s):
-            if s[i] in ["E", "e"]:
-                if not digit_seen:
+            elif ch in "+-":
+                #"6+1"
+                if symbol or digit or decimal:
+                        return False
+                symbol = True
+            elif ch in ".":
+                if decimal or e:
                     return False
-                else:
-                    return digit_seen and  self.validate(s[i+1:])
-            elif s[i] in ["+","-"]:
-                if sign_seen or digit_seen or decimal_seen:
-                    return False
-            elif s[i] == ".":
-                if  decimal_seen:
-                    return False
-                else:
-                    decimal_seen = True
-            else:
-                if not s[i].isdigit():
-                    return False
-                digit_seen = True
-            i +=1
+                decimal = True
 
-        return digit_seen
+            elif ch in "eE":
+                if not digit or e:
+                    return False
+                e = True
+                digit = False
+                symbol = False
+                decimal = False
+            else:
+
+                return False
+        return digit
+
+        
