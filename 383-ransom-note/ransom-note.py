492775d8f97eda39f645c815e237dca2ff3a2bcf
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-04-15 09:52:08 -0700

    Time: 55 ms (62.63%) | Memory: 16.8 MB (45.19%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,8 +2,8 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         hashMap= Counter(magazine)
 
-        a: 2
-        b :1
+        # a: 2
+        # b :1
 
         for ch in ransomNote:
             if ch not in hashMap:
