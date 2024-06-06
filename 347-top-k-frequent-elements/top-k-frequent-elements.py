2ff3b3aa5ef8caa7d22cb384bb262b7a8628287d
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-06-05 19:40:59 -0700

    Time: 84 ms (80.39%) | Memory: 21.2 MB (40.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,33 @@
 from collections import Counter
+from heapq import heappush, heappop, heapify
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        map = Counter(nums)
+        # map = Counter(nums)
+        map =  Counter(nums)
+        data = [ (val, key) for key ,val in map.items()]
+        heapify(data)
 
-        data = [ [] for _ in range(len(nums) + 1)]
-        print(map)
-
-        for key, val in map.items():
-            data[val].append(key)
+        while len(data) > k:
+            heappop(data)
         print(data)
 
-        ans = []
+        return [ num for(fre, num) in data]
 
-        for i in range(len(data)-1,-1,-1):
-            if len(data[i]) > 0:
-                for j in range(len(data[i])):
-                    ans.append(data[i][j])
-                    if len(ans) == k:
-                        return ans
+
+        # data = [ [] for _ in range(len(nums) + 1)]
+        # print(map)
+
+        # for key, val in map.items():
+        #     data[val].append(key)
+        # print(data)
+
+        # ans = []
+
+        # for i in range(len(data)-1,-1,-1):
+        #     if len(data[i]) > 0:
+        #         for j in range(len(data[i])):
+        #             ans.append(data[i][j])
+        #             if len(ans) == k:
+        #                 return ans
             
         
