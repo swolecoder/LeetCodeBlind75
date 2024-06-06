f49ffb7f4b93aa2214dc92447d37b653ccc64fa7
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-06-05 19:35:48 -0700

    Time: 94 ms (29.51%) | Memory: 22.4 MB (5.82%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,23 +2,21 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         map = Counter(nums)
-        data = [[] for _ in range(len(nums) + 1)]
+
+        data = [ [] for _ in range(len(nums) + 1)]
         print(map)
 
         for key, val in map.items():
             data[val].append(key)
-        
-        ans = []
-        if not nums:
-            return []
         print(data)
 
+        ans = []
+
         for i in range(len(data)-1,-1,-1):
-            if data[i]:
+            if len(data[i]) > 0:
                 for j in range(len(data[i])):
                     ans.append(data[i][j])
                     if len(ans) == k:
                         return ans
-        return []
-
+            
         
