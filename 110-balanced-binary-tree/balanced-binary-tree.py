ac6f2521c055902c5fc3811895fe67bb60df64d5
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2024-06-20 20:46:40 -0700

    Time: 42 ms (77.44%) | Memory: 17.7 MB (39.65%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,16 +7,26 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
 
-        def dfs(node):
+        def helper(node):
             if not node:
                 return [True, 0]
             
-            left = dfs(node.left)
-            right = dfs(node.right)
-            cal = left[0] and right[0] and abs(left[1]-right[1]) < 2
-            if call == False:
-                return [False, 0]
+            left = helper(node.left)
+            right = helper(node.right)
+            
+            # if not left[0] or not right[0]:
+            #     return [False, None]
+            
+            is_unbalanced = left[0] and right[0] and abs(left[1] - right[1]) < 2
+            if not is_unbalanced :
+                return [False, None]
             else:
-                return [cal, max(left[1],right[1]) +1]
-        return dfs(root)[0]
+            
+                return [is_unbalanced, max(left[1], right[1]) + 1]
+        
+        return helper(root)[0]
+
+
+
+
         
